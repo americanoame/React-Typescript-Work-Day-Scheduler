1d65def18f91d8d01a8444eb2b4c6b4691b9aa0a
Americano.Dev <andrenations@gmail.com>
2024-08-15 11:50:30 -0400

    Add Scheduler component with header and introductory text

---
+++ frontend/src/components/Scheduler.tsx
@@ -0,0 +1,17 @@
+
+
+const Scheduler = () => {
+  return (
+    <div>
+    <header className="p-5 mb-4 border-5 border-bottom border-dark text-center">
+        <h1 className="display-3">React-TypeScript</h1>
+        <h3>Work Day Scheduler</h3>
+        <p className="lead">A simple calendar app for scheduling your work day</p>
+        <p id="currentDay" className="lead"></p>
+      </header>
+      
+    </div>
+  )
+}
+
+export default Scheduler
