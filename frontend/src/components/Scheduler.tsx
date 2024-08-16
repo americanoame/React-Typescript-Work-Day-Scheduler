0ea1022d5b8ec6db225e133d16e544cbdf632751
Americano.Dev <andrenations@gmail.com>
2024-08-16 19:38:43 -0400

    Refactor Scheduler component by moving time block logic and state handling into the useEffect hook and integrating TimeBlockRow component

--- frontend/src/components/Scheduler.tsx
+++ frontend/src/components/Scheduler.tsx
@@ -1,17 +1,57 @@
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { TimeBlock, timeBlocks } from '../types';
+import { loadBlocksFromLocalStorage, saveBlockToLocalStorage } from '../localStorageUtils';
+import TimeBlockRow from '../components/TimeBlockRow';
 
 
-const Scheduler = () => {
+const Scheduler: React.FC = () => {
+  const [blocks, setBlocks] = useState<TimeBlock[]>(timeBlocks);
+
+  useEffect(() => {
+    const loadedBlocks = loadBlocksFromLocalStorage(timeBlocks);
+    setBlocks(loadedBlocks);
+
+    const updateTimeBlocks = () => {
+      const currentHour = dayjs().hour();
+      setBlocks(prevBlocks =>
+        prevBlocks.map(block => ({
+          ...block,
+          status: block.hour < currentHour ? 'past' : block.hour === currentHour ? 'present' : 'future',
+        }))
+      );
+    };
+
+    updateTimeBlocks();
+
+    const interval = setInterval(updateTimeBlocks, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleSave = (hour: number, text: string) => {
+    saveBlockToLocalStorage(hour, text);
+  };
+
+  const capitalizeFirstLetter = (text: string) => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  };
+
+  const handleChange = (hour: number, text: string) => {
+    const capitalizedText = capitalizeFirstLetter(text);
+    setBlocks(blocks.map(block => 
+      block.hour === hour ? { ...block, text: capitalizedText } : block
+    ));
+  };
+
   return (
     <div>
-    <header className="p-5 mb-4 border-5 border-bottom border-dark text-center">
-        <h1 className="display-3">React-TypeScript</h1>
-        <h3>Work Day Scheduler</h3>
-        <p className="lead">A simple calendar app for scheduling your work day</p>
-        <p id="currentDay" className="lead"></p>
-      </header>
-      
+      <div className="container-lg px-5">
+        {blocks.map(block => (
+          <TimeBlockRow key={block.hour} block={block} onSave={handleSave} onChange={handleChange} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Scheduler
+export default Scheduler;
