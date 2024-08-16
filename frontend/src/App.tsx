4a557bad9a78710e859e7f0f8fa5d0aa4bfb8cce
Americano.Dev <andrenations@gmail.com>
2024-08-16 19:32:10 -0400

    Add Header component with current day and time display; update App component to include Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
+
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
 import Scheduler from './components/Scheduler';
 
+import dayjs from 'dayjs';
+
 const App: React.FC = () => {
+  const [currentDay, setCurrentDay] = useState<string>('');
+  const [currentHour, setCurrentHour] = useState<string>('');
+
+  useEffect(() => {
+    // Set the current day when the component mounts
+    setCurrentDay(dayjs().format('MMMM D, YYYY'));
+
+    const interval = setInterval(() =>  {
+      setCurrentHour(dayjs().format('h:mm:ss A')); // Set the current hour when the component mounts
+    }, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+    
+  }, []);
+  
+
   return (
     <div className="app-container">
-      <Scheduler />
+      <Header currentDay={currentDay} currentHour={currentHour}  />
+      <Scheduler /> 
     </div>
   );
 };
