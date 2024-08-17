ac24e5f9f8f44b58b3f420e922353866014d1c66
Americano.Dev <andrenations@gmail.com>
2024-08-16 21:59:07 -0400

    Update Header component to display reminder text
    - Added reminderText prop to Header component.
    - Displayed reminder text conditionally in the header if it exists.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,13 @@
 interface HeaderProps {
   currentDay: string;
   currentHour: string;
+  reminderText: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ currentDay, currentHour }) => (
+const Header: React.FC<HeaderProps> = ({ currentDay, currentHour, reminderText }) => (
   <header className="p-1 text-center">
     <h1 className="display-6">React TypeScript</h1>
-    <h3>Daly Task Alert</h3>
+    <h3>Scheduler Alert</h3>
     <p className="lead">Get an alert 30 minutes before any scheduling</p>
     <p id="currentDay" className="lead">
       {currentDay}
@@ -14,6 +15,7 @@ const Header: React.FC<HeaderProps> = ({ currentDay, currentHour }) => (
     <p id="currentHour" className="lead">
       {currentHour}
     </p>
+    {reminderText && <p className="lead">{reminderText}</p>} 
   </header>
 );
 
